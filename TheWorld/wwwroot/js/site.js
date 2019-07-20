21d994513c53147d96f77f87dfc85f636975b568
Kosyakoff <Kosyakoff@CUCUMBER>
2019-07-21 00:28:53 +0700

    Finished first javascript

---
+++ TheWorld/wwwroot/js/site.js
@@ -0,0 +1,35 @@
+﻿
+(function () {
+
+    //var ele = $("#username");
+    //ele.text("Kosyakoff");
+
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.css("background-color", "#888");
+    //});
+    //main.on("mouseleave", function () {
+    //    main.css("background-color", "");
+    //});
+
+    //$(".menu li a").on("click", function () {
+    //    alert($(this).text());
+    //    return false;
+    //});
+
+    var sideBarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        sideBarAndWrapper.toggleClass("hide-sidebar");
+
+        if (sideBarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show sidebar");
+        }
+        else {
+            $(this).text("Hide sidebar");
+        }
+
+    });
+
+})();
+
