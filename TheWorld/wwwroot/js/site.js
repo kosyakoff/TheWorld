a0c7981fc7a6ba6fbf3b9999052e6c08334b7f0e
Kosyakoff <Kosyakoff@CUCUMBER>
2019-07-31 22:26:01 +0700

    Added layout view, start page, and moved logic to services

--- TheWorld/wwwroot/js/site.js
+++ TheWorld/wwwroot/js/site.js
@@ -1,22 +1,6 @@
 ﻿
 (function () {
 
-    //var ele = $("#username");
-    //ele.text("Kosyakoff");
-
-    //var main = $("#main");
-    //main.on("mouseenter", function () {
-    //    main.css("background-color", "#888");
-    //});
-    //main.on("mouseleave", function () {
-    //    main.css("background-color", "");
-    //});
-
-    //$(".menu li a").on("click", function () {
-    //    alert($(this).text());
-    //    return false;
-    //});
-
     var sideBarAndWrapper = $("#sidebar,#wrapper");
 
     $("#sidebarToggle").on("click", function () {
@@ -30,6 +14,22 @@
         }
 
     });
+
+//    var main = $("#main");
+//    main.on("mouseenter", function () {
+//        main.css("background-color", "#888");
+//    });
+//    main.on("mouseleave", function () {
+//        main.css("background-color", "");
+//    });
+//
+//    $(".menu li a").on("click", function () {
+//        alert($(this).text());
+//        return false;
+//    });
+//
+//    var ele = $("#username");
+//    ele.text("Kosyakoff");
 
 })();
 
